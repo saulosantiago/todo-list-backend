354c30f65a58077e65e14f26b5ee8a03e88418a3
Saulo <saulo8732n@gmail.com>
2021-07-10 00:44:34 -0300

    feat: add signup usecase

---
+++ src/domain/usecases/signup-usecase.js
@@ -0,0 +1,16 @@
+module.exports = class SignUpUseCase {
+  constructor ({ createUserRepository, updateAccessTokenRepository, encrypter, tokenGenerator } = {}) {
+    this.createUserRepository = createUserRepository
+    this.updateAccessTokenRepository = updateAccessTokenRepository
+    this.encrypter = encrypter
+    this.tokenGenerator = tokenGenerator
+  }
+
+  async signUp (name, email, password) {
+    const hashedPassword = await this.encrypter.hash(password)
+    const user = await this.createUserRepository.create(name, email, hashedPassword)
+    const accessToken = await this.tokenGenerator.generate(user._id)
+    await this.updateAccessTokenRepository.update(user._id, accessToken)
+    return { user, accessToken }
+  }
+}
