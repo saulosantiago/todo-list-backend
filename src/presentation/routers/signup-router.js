c43428afa7a02e8a23e3ef23e470f81317201f5a
Saulo <saulo8732n@gmail.com>
2021-07-10 00:07:58 -0300

    feat: add signup router

---
+++ src/presentation/routers/signup-router.js
@@ -0,0 +1,31 @@
+const HttpResponse = require('../helpers/http-response')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+
+module.exports = class SignUpRouter {
+  constructor ({ signUpUseCase, emailValidator } = {}) {
+    this.signUpUseCase = signUpUseCase
+    this.emailValidator = emailValidator
+  }
+
+  async route (httpRequest) {
+    try {
+      const { name, email, password } = httpRequest.body
+      if (!name) {
+        return HttpResponse.badRequest(new MissingParamError('name'))
+      }
+      if (!email) {
+        return HttpResponse.badRequest(new MissingParamError('email'))
+      }
+      if (!this.emailValidator.isValid(email)) {
+        return HttpResponse.badRequest(new InvalidParamError('email'))
+      }
+      if (!password) {
+        return HttpResponse.badRequest(new MissingParamError('password'))
+      }
+      const result = await this.signUpUseCase.signUp(name, email, password)
+      return HttpResponse.ok(result)
+    } catch (error) {
+      return HttpResponse.serverError()
+    }
+  }
+}
