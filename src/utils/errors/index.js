680f9b49bf02385f5fd616c2210ab816c5f8c6e2
Saulo <saulo8732n@gmail.com>
2021-07-10 11:47:23 -0300

    feat: add create user repository

--- src/utils/errors/index.js
+++ src/utils/errors/index.js
@@ -1,7 +1,9 @@
 const MissingParamError = require('./missing-param-error')
 const InvalidParamError = require('./invalid-param-error')
+const EmailAlreadyExistsError = require('./email-already-exists-error')
 
 module.exports = {
   MissingParamError,
-  InvalidParamError
+  InvalidParamError,
+  EmailAlreadyExistsError
 }
