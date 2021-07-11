2186677eaf355f866f1bb9abc856bf45d847fb83
Saulo <saulo8732n@gmail.com>
2021-07-11 01:45:32 -0300

    fix: remove unused variable

--- src/main/composers/user/sign-out-router-composer.js
+++ src/main/composers/user/sign-out-router-composer.js
@@ -1,7 +1,6 @@
 const SignOutRouter = require('../../../presentation/routers/user/sign-out-router')
 const SignOutUseCase = require('../../../domain/usecases/user/sign-out-usecase')
 const UpdateAccessTokenRepository = require('../../../infra/repositories/update-access-token-repository')
-const env = require('../../config/env')
 
 module.exports = class SignOutRouterComposer {
   static compose () {
